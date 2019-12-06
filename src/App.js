a99035c99af999af2627f40d867bbc738022a6a6
Christian Lehr <christian_lehr.1@gmx.de>
2019-12-06 22:08:00 +0100

    add input for last episode

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends React.Component {
     return (
       <div className="vh-100 dt w-100">
         <div className="dtc v-mid tc">
-          <p className="avenir i b">Last Episode was: {this.state.lastEpisode}</p>
+          <p className="avenir i b">Last Episode was: <input style={{ width: '50px' }} value={this.state.lastEpisode} /></p>
           <p className="avenir i b">Enter episode you like to watch:</p>
           <input onChange={this.setEpisodeToWatch}></input>
           <a target="_blank" rel="noopener noreferrer" href={'http://onepiece-tube.com/folge/' + this.state.episodeToWatch} className="f6 link dim ph3 pv1 mb2 dib white bg-light-purple b avenir i">Start Watching!</a>
