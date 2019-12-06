ca7e6465738d0df7ca93e2992c730d7d1e063c60
Christian Lehr <christian_lehr.1@gmx.de>
2019-12-06 22:37:29 +0100

    add anime name before input field

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends React.Component {
     super(props);
     this.state = {
       episodeToWatch: '',
-      lastEpisode: ''
+      lastEpisode: '',
+      anime: ''
     };
     this.setEpisodeToWatch = this.setEpisodeToWatch.bind(this)
     this.setLastWatchedEpisode = this.setLastWatchedEpisode.bind(this)
@@ -17,7 +18,7 @@ class App extends React.Component {
     return (
       <div className="vh-100 dt w-100">
         <div className="dtc v-mid tc">
-          <p className="avenir i b">Last Episode was: <input type="text" value={this.state.lastEpisode} onChange={this.setLastWatchedEpisode} style={{ width: '50px' }} /></p>
+          <p className="avenir i b">Last Episode of {this.state.anime} was: <input type="text" value={this.state.lastEpisode} onChange={this.setLastWatchedEpisode} style={{ width: '50px' }} /></p>
           <p className="avenir i b">Enter episode you like to watch:</p>
           <input onChange={this.setEpisodeToWatch}></input>
           <a target="_blank" rel="noopener noreferrer" href={'http://onepiece-tube.com/folge/' + this.state.episodeToWatch} className="f6 link dim ph3 pv1 mb2 dib white bg-light-purple b avenir i">Start Watching!</a>
@@ -29,6 +30,7 @@ class App extends React.Component {
 
   componentDidMount() {
     firebase.database().ref('last_episode').child("one_piece").once('value', storedValue => this.setState({ lastEpisode: storedValue.val() }));
+    firebase.database().ref('last_episode').child("one_piece").once('value', storedValue => this.setState({ anime: storedValue.key }));
   }
 
   setEpisodeToWatch(event) {
