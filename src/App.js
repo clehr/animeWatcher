a631ca86753fc2a41fada50f0efdc1f94ae7f794
Christian Lehr <christian_lehr.1@gmx.de>
2019-12-06 22:15:56 +0100

    set state of input field

--- src/App.js
+++ src/App.js
@@ -7,16 +7,17 @@ class App extends React.Component {
     super(props);
     this.state = {
       episodeToWatch: 903,
-      lastEpisode: ""
+      lastEpisode: ''
     };
     this.setEpisodeToWatch = this.setEpisodeToWatch.bind(this)
+    this.setLastWatchedEpisode = this.setLastWatchedEpisode.bind(this)
   }
 
   render() {
     return (
       <div className="vh-100 dt w-100">
         <div className="dtc v-mid tc">
-          <p className="avenir i b">Last Episode was: <input style={{ width: '50px' }} value={this.state.lastEpisode} /></p>
+          <p className="avenir i b">Last Episode was: <input type="text" value={this.state.lastEpisode} onChange={this.setLastWatchedEpisode} style={{ width: '50px' }} /></p>
           <p className="avenir i b">Enter episode you like to watch:</p>
           <input onChange={this.setEpisodeToWatch}></input>
           <a target="_blank" rel="noopener noreferrer" href={'http://onepiece-tube.com/folge/' + this.state.episodeToWatch} className="f6 link dim ph3 pv1 mb2 dib white bg-light-purple b avenir i">Start Watching!</a>
@@ -35,6 +36,14 @@ class App extends React.Component {
 
     this.setState({
       episodeToWatch: inputValue
+    })
+  }
+
+  setLastWatchedEpisode(event) {
+    const inputValue = event.target.value;
+
+    this.setState({
+      lastEpisode: inputValue
     })
   }
 
