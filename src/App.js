7f489ae419c3de7aed9bb32cf67508761ac8e131
Christian Lehr <christian_lehr.1@gmx.de>
2019-12-06 21:24:25 +0100

    set episode to watch from input field

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="vh-100 dt w-100">
-      <div className="dtc v-mid tc">
-        <p className="avenir i">Last Episode was: 905</p>
-        <p className="avenir i">Enter episode you like to watch:</p>
-        <input></input>
-        <a className="f6 link dim ph3 pv1 mb2 dib white bg-light-purple">Start Watching!</a>
-      </div>
-    </div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { episodeToWatch: 903 };
+    this.setEpisodeToWatch = this.setEpisodeToWatch.bind(this)
+  }
 
-  );
+  render() {
+    return (
+      <div className="vh-100 dt w-100">
+        <div className="dtc v-mid tc">
+          <p className="avenir i b">Last Episode was: 905</p>
+          <p className="avenir i b">Enter episode you like to watch:</p>
+          <input onChange={this.setEpisodeToWatch}></input>
+          <a target="_blank" rel="noopener noreferrer" href={'http://onepiece-tube.com/folge/' + this.state.episodeToWatch} className="f6 link dim ph3 pv1 mb2 dib white bg-light-purple b avenir i">Start Watching!</a>
+        </div>
+      </div>
+
+    );
+  }
+
+  setEpisodeToWatch(event) {
+    const inputValue = event.target.value;
+
+    this.setState({
+      episodeToWatch: inputValue
+    })
+  }
+
 }
 
 export default App;
