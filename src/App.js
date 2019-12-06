014d58fddc5ca9e8b293d4702e701f2a5a89c9f3
Christian Lehr <christian_lehr.1@gmx.de>
2019-12-06 23:59:13 +0100

    visit site on input submit

--- src/App.js
+++ src/App.js
@@ -18,9 +18,12 @@ class App extends React.Component {
     return (
       <div className="vh-100 dt w-100">
         <div className="dtc v-mid tc">
+          <p className="avenir b">All episodes can be found <a target="_blank" rel="noopener noreferrer" href="http://onepiece-tube.com/episoden-streams">here</a></p>
           <p className="avenir i b">Last Episode of {this.state.anime} was: <input type="text" value={this.state.lastEpisode} onChange={this.setLastWatchedEpisode} style={{ width: '50px' }} /></p>
           <p className="avenir i b">Enter episode you like to watch:</p>
-          <input onChange={this.setEpisodeToWatch}></input>
+          <form className="pb3" target="_blank" action={'http://onepiece-tube.com/folge/' + this.state.episodeToWatch}>
+            <input onChange={this.setEpisodeToWatch} style={{ width: '50px' }} />
+          </form>
           <a target="_blank" rel="noopener noreferrer" href={'http://onepiece-tube.com/folge/' + this.state.episodeToWatch} className="f6 link dim ph3 pv1 mb2 dib white bg-light-purple b avenir i">Start Watching!</a>
         </div>
       </div>
