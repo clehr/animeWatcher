900711de19a5f1b287c187fe8feedb0a3ba229e3
Christian Lehr <christian_lehr.1@gmx.de>
2019-12-08 14:56:20 +0100

    add password input

--- src/App.js
+++ src/App.js
@@ -12,60 +12,64 @@ class App extends React.Component {
       lastEpisode: '',
       lastEpisodeFairyTail: '',
       lastEpisodeBoruto: '',
-      anime: '',
       background: '',
-      activeSeries: 'fairy_tail'
+      activeSeries: 'fairy_tail',
+      auth: 'IWantToWatchAnime',
+      password: ''
     };
     this.setEpisodeToWatch = this.setEpisodeToWatch.bind(this)
     this.setLastWatchedEpisode = this.setLastWatchedEpisode.bind(this)
     this.setActiveSeries = this.setActiveSeries.bind(this)
+    this.setPassword = this.setPassword.bind(this)
   }
 
   render() {
     return (
       <div className={"vh-100 dt w-100 " + (this.state.background)}>
-        <div className="dtc v-mid tc">
-          <select onChange={this.setActiveSeries} name="series">
-            <option value="fairy_tail">Fairy Tail</option>
-            <option value="boruto">Boruto</option>
-            <option value="one_piece">One Piece</option>
-          </select>
+        <input onChange={this.setPassword} />
+        {this.state.password == this.state.auth &&
+          <div className="dtc v-mid tc">
+            <select onChange={this.setActiveSeries} name="series">
+              <option value="fairy_tail">Fairy Tail</option>
+              <option value="boruto">Boruto</option>
+              <option value="one_piece">One Piece</option>
+            </select>
 
-          {this.state.activeSeries === 'fairy_tail' && <div>
-            <p className="avenir b">All episodes can be found <a target="_blank" rel="noopener noreferrer" href="http://fairytail-tube.org/episoden-streams">here</a></p>
-            <p className="avenir i b">Last Episode of {this.state.activeSeries} was:
+            {this.state.activeSeries === 'fairy_tail' && <div>
+              <p className="avenir b">All episodes can be found <a target="_blank" rel="noopener noreferrer" href="http://fairytail-tube.org/episoden-streams">here</a></p>
+              <p className="avenir i b">Last Episode of {this.state.activeSeries} was:
              <input type="text" value={this.state.lastEpisodeFairyTail} onChange={this.setLastWatchedEpisode} style={{ width: '50px' }} /></p>
-            <p className="avenir i b">Enter episode you like to watch: </p>
-            <form className="pb3" target="_blank" action={'http://fairytail-tube.org/folge/' + this.state.episodeToWatchFairyTail}>
-              <input value={this.state.episodeToWatchFairyTail} onChange={this.setEpisodeToWatch} style={{ width: '50px' }} />
-            </form>
-            <a target="_blank" rel="noopener noreferrer" href={'http://fairytail-tube.org/folge/' + this.state.episodeToWatchFairyTail} className="f6 link dim ph3 pv1 mb2 dib white bg-light-purple b avenir i">Start Watching!</a>
-          </div>
-          }
+              <p className="avenir i b">Enter episode you like to watch: </p>
+              <form className="pb3" target="_blank" action={'http://fairytail-tube.org/folge/' + this.state.episodeToWatchFairyTail}>
+                <input value={this.state.episodeToWatchFairyTail} onChange={this.setEpisodeToWatch} style={{ width: '50px' }} />
+              </form>
+              <a target="_blank" rel="noopener noreferrer" href={'http://fairytail-tube.org/folge/' + this.state.episodeToWatchFairyTail} className="f6 link dim ph3 pv1 mb2 dib white bg-light-purple b avenir i">Start Watching!</a>
+            </div>
+            }
 
-          {this.state.activeSeries === 'boruto' && <div>
-            <p className="avenir b">All episodes can be found <a target="_blank" rel="noopener noreferrer" href="http://anisenpai.net/boruto/">here</a></p>
-            <p className="avenir i b">Last Episode of {this.state.activeSeries} was: <input type="text" value={this.state.lastEpisodeBoruto} onChange={this.setLastWatchedEpisode} style={{ width: '50px' }} /></p>
-            <p className="avenir i b">Enter episode you like to watch:</p>
-            <form className="pb3" target="_blank" action={'http://anisenpai.net/boruto-folge-' + this.state.episodeToWatchBoruto + '-ger-sub/'}>
-              <input value={this.state.episodeToWatchBoruto} onChange={this.setEpisodeToWatch} style={{ width: '50px' }} />
-            </form>
-            <a target="_blank" rel="noopener noreferrer" href={'http://anisenpai.net/boruto-folge-' + this.state.episodeToWatchBoruto + '-ger-sub/'} className="f6 link dim ph3 pv1 mb2 dib white bg-light-purple b avenir i">Start Watching!</a>
-          </div>
-          }
+            {this.state.activeSeries === 'boruto' && <div>
+              <p className="avenir b">All episodes can be found <a target="_blank" rel="noopener noreferrer" href="http://anisenpai.net/boruto/">here</a></p>
+              <p className="avenir i b">Last Episode of {this.state.activeSeries} was: <input type="text" value={this.state.lastEpisodeBoruto} onChange={this.setLastWatchedEpisode} style={{ width: '50px' }} /></p>
+              <p className="avenir i b">Enter episode you like to watch:</p>
+              <form className="pb3" target="_blank" action={'http://anisenpai.net/boruto-folge-' + this.state.episodeToWatchBoruto + '-ger-sub/'}>
+                <input value={this.state.episodeToWatchBoruto} onChange={this.setEpisodeToWatch} style={{ width: '50px' }} />
+              </form>
+              <a target="_blank" rel="noopener noreferrer" href={'http://anisenpai.net/boruto-folge-' + this.state.episodeToWatchBoruto + '-ger-sub/'} className="f6 link dim ph3 pv1 mb2 dib white bg-light-purple b avenir i">Start Watching!</a>
+            </div>
+            }
 
-          {this.state.activeSeries === 'one_piece' && <div>
-            <p className="avenir b">All episodes can be found <a target="_blank" rel="noopener noreferrer" href="http://onepiece-tube.com/episoden-streams">here</a></p>
-            <p className="avenir i b">Last Episode of {this.state.activeSeries} was: <input type="text" value={this.state.lastEpisode} onChange={this.setLastWatchedEpisode} style={{ width: '50px' }} /></p>
-            <p className="avenir i b">Enter episode you like to watch:</p>
-            <form className="pb3" target="_blank" action={'http://onepiece-tube.com/folge/' + this.state.episodeToWatch}>
-              <input value={this.state.episodeToWatch} onChange={this.setEpisodeToWatch} style={{ width: '50px' }} />
-            </form>
-            <a target="_blank" rel="noopener noreferrer" href={'http://onepiece-tube.com/folge/' + this.state.episodeToWatch} className="f6 link dim ph3 pv1 mb2 dib white bg-light-purple b avenir i">Start Watching!</a>
+            {this.state.activeSeries === 'one_piece' && <div>
+              <p className="avenir b">All episodes can be found <a target="_blank" rel="noopener noreferrer" href="http://onepiece-tube.com/episoden-streams">here</a></p>
+              <p className="avenir i b">Last Episode of {this.state.activeSeries} was: <input type="text" value={this.state.lastEpisode} onChange={this.setLastWatchedEpisode} style={{ width: '50px' }} /></p>
+              <p className="avenir i b">Enter episode you like to watch:</p>
+              <form className="pb3" target="_blank" action={'http://onepiece-tube.com/folge/' + this.state.episodeToWatch}>
+                <input value={this.state.episodeToWatch} onChange={this.setEpisodeToWatch} style={{ width: '50px' }} />
+              </form>
+              <a target="_blank" rel="noopener noreferrer" href={'http://onepiece-tube.com/folge/' + this.state.episodeToWatch} className="f6 link dim ph3 pv1 mb2 dib white bg-light-purple b avenir i">Start Watching!</a>
+            </div>
+            }
           </div>
-          }
-
-        </div>
+        }
       </div>
     );
   }
@@ -96,6 +100,14 @@ class App extends React.Component {
     this.setState({
       background: backgrounds[Math.floor(Math.random() * backgrounds.length)]
     });
+  }
+
+  setPassword(event) {
+    const enteredPassword = event.target.value;
+
+    this.setState({
+      password: enteredPassword
+    })
   }
 
   setActiveSeries(event) {
